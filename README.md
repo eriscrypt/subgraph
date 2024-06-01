ff8092355e6b2ed398d3d1f6158d3e4eb61aa024
eriscrypt <eriscrypt@gmail.com>
2024-06-01 14:49:03 +0300

    update: init

---
+++ README.md
@@ -0,0 +1,36 @@
+# How to use
+
+## Installation
+
+```bash
+yarn install
+```
+
+## Before running the app
+
+```bash
+$ yarn remove-local
+$ docker-compose down
+```
+
+## Running the app
+
+```bash
+# Build
+$ yarn build && yarn codegen
+
+# Run docker
+$ docker-compose up -d
+
+# Create ipfs and database
+$ yarn create-local
+$ yarn deploy-local
+```
+
+## Important
+
+### Make sure
+
+1. You have docker installed
+2. You have node installed
+3. You have provided the correct RPC
--- src/stablecoin-farm.ts
+++ src/stablecoin-farm.ts
@@ -1,202 +1,68 @@
 import {
-  EmergencyWithdraw as EmergencyWithdrawEvent,
-  ExitEarly as ExitEarlyEvent,
-  Locked as LockedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Paused as PausedEvent,
-  RenounceManagerRole as RenounceManagerRoleEvent,
-  SetManager as SetManagerEvent,
   Staked as StakedEvent,
-  Unpaused as UnpausedEvent,
-  Vested as VestedEvent,
   Withdraw as WithdrawEvent,
-  WithdrawVesting as WithdrawVestingEvent
-} from "../generated/StablecoinFarm/StablecoinFarm"
-import {
-  EmergencyWithdraw,
-  ExitEarly,
-  Locked,
-  OwnershipTransferred,
-  Paused,
-  RenounceManagerRole,
-  SetManager,
-  Staked,
-  Unpaused,
-  Vested,
-  Withdraw,
-  WithdrawVesting
-} from "../generated/schema"
+} from '../generated/StablecoinFarm/StablecoinFarm';
+import { Staked, TotalStats, Withdraw } from '../generated/schema';
+import { BigInt } from '@graphprotocol/graph-ts';
 
-export function handleEmergencyWithdraw(event: EmergencyWithdrawEvent): void {
-  let entity = new EmergencyWithdraw(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.pid = event.params.pid
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleExitEarly(event: ExitEarlyEvent): void {
-  let entity = new ExitEarly(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleLocked(event: LockedEvent): void {
-  let entity = new Locked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.pid = event.params.pid
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaused(event: PausedEvent): void {
-  let entity = new Paused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRenounceManagerRole(
-  event: RenounceManagerRoleEvent
-): void {
-  let entity = new RenounceManagerRole(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSetManager(event: SetManagerEvent): void {
-  let entity = new SetManager(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
+/**
+ * Handle Staked event
+ * @param event
+ */
 export function handleStaked(event: StakedEvent): void {
   let entity = new Staked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.pid = event.params.pid
-  entity.amount = event.params.amount
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  );
+  entity.user = event.params.user;
+  entity.pid = event.params.pid;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  // Update total stats
+  let stats = TotalStats.load('totalStats');
+  if (stats == null) {
+    stats = new TotalStats('totalStats');
+    stats.totalStaked = event.params.amount;
+    stats.totalWithdraw = BigInt.fromI32(0);
+  } else {
+    stats.totalStaked = stats.totalStaked.plus(event.params.amount);
+  }
+
+  stats.save();
 }
 
-export function handleUnpaused(event: UnpausedEvent): void {
-  let entity = new Unpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleVested(event: VestedEvent): void {
-  let entity = new Vested(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.pid = event.params.pid
-  entity.amount = event.params.amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
+/**
+ * Handle Withdraw event
+ * @param event
+ */
 export function handleWithdraw(event: WithdrawEvent): void {
   let entity = new Withdraw(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.pid = event.params.pid
-  entity.amount = event.params.amount
+    event.transaction.hash.concatI32(event.logIndex.toI32()),
+  );
+  entity.user = event.params.user;
+  entity.pid = event.params.pid;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
-}
+  entity.save();
 
-export function handleWithdrawVesting(event: WithdrawVestingEvent): void {
-  let entity = new WithdrawVesting(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.amount = event.params.amount
+  // Update total stats
+  let stats = TotalStats.load('totalStats');
+  if (stats == null) {
+    stats = new TotalStats('totalStats');
+    stats.totalStaked = BigInt.fromI32(0);
+    stats.totalWithdraw = event.params.amount;
+  } else {
+    stats.totalWithdraw = stats.totalWithdraw.plus(event.params.amount);
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  stats.save();
 }
